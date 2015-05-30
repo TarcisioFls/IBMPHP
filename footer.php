2f5a4db806abf1d4b8e002562548712b56150bf3
anedavino <anne_carol.93@hotmail.com>
2015-05-30 11:08:28 -0300

    Merge origin/master
    Conflicts:
    	index.php
    	menu.php
    	nbproject/private/private.xml

---
+++ footer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <script src="../js/jquery-2.1.3.js"></script>
+        <script src="../js/bootstrap.min.js"></script> 
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    </head>
+    <body>
+       <div id="footer">
+			<div id="footerbg">
+				<div class="wrap">
+	
+					<!-- /footer links --> 
+					 <H2 CLASS="RODAPÉ1">&nbsp Endereço</H2> 
+					<H3 CLASS="RODAPÉ">Rua do Rosário, Muribeca dos Gruararapes<br/>
+					Jaboatão dos Guararapes - PE</H3>
+					
+					<p class="nossaEmpresa"> IT Solutio </p>
+					
+					<div class="clear"></div>
+				</div>
+			</div>
+		</div>
+    </body>
+</html>
+
+
+
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    include_once './menu.php';
+    include_once 'menu.php';
 ?>
 <!DOCTYPE html>
 <!--
@@ -15,6 +15,7 @@ and open the template in the editor.
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <script src="js/jquery-2.1.3.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/estilo.css">
         <style>
             .carousel-inner > .item > img,
             .carousel-inner > .item > a > img {
@@ -70,12 +71,192 @@ and open the template in the editor.
                         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
                       </a>
+                <div> <!--div total-->
+	<div class="prop"><!--div propaganda-->
+	<div><img src="img/thumb3.png" alt="Thumb" width="171px" height="400"/></div>
+	<div><img src="img/thumb3.png" alt="Thumb" width="171px" height="400"/></div>
+	</div>
+	<div class="corposite"> <!--div corpo do site-->
+		
+	
+	<div class="evento"><!--div eventos-->
+		<div class="container">
+  <h2>Eventos do Mês</h2>
+ 
+  <ul class="nav nav-tabs">
+    <li><a data-toggle="tab" href="#menu12">JAN</a></li>
+    <li><a data-toggle="tab" href="#menu1">FEV</a></li>
+    <li><a data-toggle="tab" href="#menu2">MAR</a></li>
+    <li><a data-toggle="tab" href="#menu3">ABR</a></li>
+    <li><a data-toggle="tab" href="#menu4">MAI</a></li>
+    <li><a data-toggle="tab" href="#menu5">JUN</a></li>
+    <li><a data-toggle="tab" href="#menu6">JUL</a></li>
+    <li><a data-toggle="tab" href="#menu7">AGOS</a></li>
+    <li><a data-toggle="tab" href="#menu8">SET</a></li>
+    <li><a data-toggle="tab" href="#menu9">OUT</a></li>
+    <li><a data-toggle="tab" href="#menu10">NOV</a></li>
+    <li><a data-toggle="tab" href="#menu11">DEZ</a></li>
+    
+  </ul>
+
+  <div class="tab-content">
+    <div id="menu12" class="tab-pane fade">
+      <h3>JANEIRO</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="menu1" class="tab-pane fade">
+      <h3>FEVEREIRO</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+      <h3>MARÇO</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>ABRIL</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="menu4" class="tab-pane fade">
+      <h3>MAIO</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu5" class="tab-pane fade">
+      <h3>JUNHO</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="menu6" class="tab-pane fade">
+      <h3>JULHO</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="menu7" class="tab-pane fade">
+      <h3>MARÇO</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu8" class="tab-pane fade">
+      <h3>ABRIL</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="menu9" class="tab-pane fade">
+      <h3>MAIO</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu10" class="tab-pane fade">
+      <h3>JUNHO</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="menu11" class="tab-pane fade">
+      <h3>MAIO</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+   </div>
+</div>
+	</div>
+	<div><!--div videos-->
+		<div class="video">
+                <h4 class="h4">Videos</h4>
+                <br/>
+                <div id="primeirovideo">
+                <iframe width="342" height="300" src="https://www.youtube.com/embed/4g_h0R7iVXA" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <br/>
+									<!--https://www.youtube.com/watch?v=4g_h0R7iVXA		
+									http://www.ibmemorialsj.com.br/      site de igreja referencia-->
+                <div id="ultimovideo">
+				<iframe width="342" height="300" src="https://www.youtube.com/embed/ljHqUVBbwJ8" frameborder="0" allowfullscreen></iframe>
+        		</div>
+        </div>
+	</div>
+	<div class="nhd"> <!--div noticias, horario do culto, downloads-->
+			
+					<div class="horariodoculto"> <!--div horário do culto-->
+						<h4 class="h4">Hórario dos Cultos</h4>
+						</br>
+					
+						<p> 
+						<h6 class="DiaCulto_h6">Domingo:</h6>
+						<h5 class="DiaCulto_h6">09:00hs – EBD</br> 19:00hs – Culto de Adoração</h5>
+						<h6 class="DiaCulto_h6">Terça:</h6>
+						<h5 class="DiaCulto_h5">15:00hs - Culto Espitual</h5>
+						<h6 class="DiaCulto_h6">Quarta:</h6>
+						<h5 class="DiaCulto_h5">19:00hs – Culto de Oração</h5>
+						<h6 class="DiaCulto_h6">Quinta:</h6>
+						<h5 class="DiaCulto_h5">15:00hs - Culto da 3ªIdade</h5>
+						<h6 class="DiaCulto_h6">Sábado:</h6>
+						<h5 class="DiaCulto_h5">15:00hs - Culto Infantil</br>19:oohs – Culto de Jovem</h5>
+						</p>
+					</div>  <!--fim div horário do culto-->
+				
+					<div class="downloads"> <!--div downloads-->
+						<h4 class="h4">Downloads</h4>
+						
+                        <a href="#" class="link">Boletim </a><br/><br/>
+                        <a href="#" class="link">Músicas</a></p>
+						
+					</div> <!--fim div downloads-->
+				
+					<div class="noticias"> <!--div notícias-->
+						<h4 class="h4">Noticias</h4>
+						 - Sábado 18 de Abril de 2015 não haverá culto Jovem.<br/> - No Próximo sábado 25 voltaremos as atividades normais.</p>
+					</div>  <!--fim div noticias-->
+                   
+					
+			
+	</div>  <!--fim div noticias, horario do culto, downloads-->
+
+		<div class="projetos"> <!--div projetos-->
+	<div class="projeto1">
+		<h4>Culto Jovem</h4>
+							<div class="photo">
+								<a href="#approach"><img src="img/thumb1.png" alt="Thumb" width="171px" height="100"/></a>
+							</div>
+							<p class="P_GERAL">Eu vos escrevi, jovens, porque sois fortes, e a palavra de Deus está em vós, e já vencestes o maligno.(1 João 2:14)<br/>Culto Todos os Domingos às 09h00 </p>
+	</div>
+	<div class="projeto2">
+		<h4>Escola Bíblica (EBD)</h4>
+							<div class="photo">
+								<a href="#methods"><img src="img/thumb2.png" alt="Thumb" width="180px" height="100"/></a>
+							</div>
+							<p class="P_GERAL">Bem-aventurado o homem que não anda segundo o conselho dos ímpios, nem se detém no caminho dos pecadores, 
+							nem se assenta na roda dos escarnecedores.
+							Antes tem o seu prazer na lei do Senhor, e na sua lei medita de dia e de noite.(Salmos 1:1-2)<br/> Todos os Domingos às 09h00</p>
+							<p class="more"><a href="#"> &nbsp </a></p>
+	</div>
+	<div class="projeto3">
+		<h4>Culto de Oração</h4>
+							<div class="photo">
+								<a href="#results"><img src="img/thumb3.png" alt="Thumb" width="171px" height="100"/></a>
+							</div>
+							<p class="P_GERAL">“Vinde, adoremos e prostremo-nos; ajoelhemo-nos diante do Senhor, que nos criou” (Sl 95.6)<br/> Cultos todas as Quartas feiras às 19h00</p>
+							<p class="more"><a href="#"> &nbsp </a></p>
+	</div>
+	
+</div><!--fim div projetos-->
+	</div> <!--fim div corpo do site-->
+
+		
+	</div> <!--fim div total-->
+
+</div> <!--fim div carrousel-->
+		
+        </div> <!--fim div container-->
                     </div>
                 </div>
-            
-        </section>
+
+            </section>
+
+        
         <?php
         // put your code here
+        include_once 'mapasite.php';
         ?>
+        </br>
+            
+
     </body>
+
 </html>
+       <?php
+        include_once 'footer.php';
+        ?>
+
+
